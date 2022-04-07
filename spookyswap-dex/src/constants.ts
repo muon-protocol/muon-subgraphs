8d5e7632dc66dfeba2ee1927f9e86ca7b27a4d62
Shayan <shayan5420@yahoo.com>
2022-04-07 10:40:23 +0430

    Add SpookySwap subgraph

---
+++ spookyswap-dex/src/constants.ts
@@ -0,0 +1,8 @@
+import { BigDecimal, BigInt } from "@graphprotocol/graph-ts"
+
+export const FACTORY_ADDRESS = '0x152eE697f2E276fA89E96742e9bB9aB1F2E61bE3'
+export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
+export let ZERO_BI = BigInt.fromI32(0)
+export let ONE_BI = BigInt.fromI32(1)
+export let ZERO_BD = BigDecimal.fromString('0')
+export let ONE_BD = BigDecimal.fromString('1')
