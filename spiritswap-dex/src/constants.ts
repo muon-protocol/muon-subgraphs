c07b7bed312dcac0fb151272cb4b022dd99e4cd7
Shayan <shayan5420@yahoo.com>
2022-04-07 11:44:01 +0430

    Add SpiritSwap subgraph

---
+++ spiritswap-dex/src/constants.ts
@@ -0,0 +1,8 @@
+import { BigDecimal, BigInt } from "@graphprotocol/graph-ts"
+
+export const FACTORY_ADDRESS = '0xEF45d134b73241eDa7703fa787148D9C9F4950b0'
+export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
+export let ZERO_BI = BigInt.fromI32(0)
+export let ONE_BI = BigInt.fromI32(1)
+export let ZERO_BD = BigDecimal.fromString('0')
+export let ONE_BD = BigDecimal.fromString('1')
