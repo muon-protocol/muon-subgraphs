6ea291163945988bcd266af92a8bd083b27fd1ef
Shayan <shayan5420@yahoo.com>
2022-07-07 14:18:57 +0430

    Minimal version

---
+++ pancakeswap-dex/src/constants.ts
@@ -0,0 +1,8 @@
+import { BigDecimal, BigInt } from "@graphprotocol/graph-ts"
+
+export const FACTORY_ADDRESS = '0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73'
+export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
+export let ZERO_BI = BigInt.fromI32(0)
+export let ONE_BI = BigInt.fromI32(1)
+export let ZERO_BD = BigDecimal.fromString('0')
+export let ONE_BD = BigDecimal.fromString('1')
