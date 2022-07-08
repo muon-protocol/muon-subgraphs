d2c4cc15a35349a84e2cfb155fa3aa8c654a62d0
Shayan <shayan5420@yahoo.com>
2022-07-08 13:44:33 +0430

    TraderJoe subgraph

---
+++ traderjoe-dex/src/constants.ts
@@ -0,0 +1,8 @@
+import { BigDecimal, BigInt } from "@graphprotocol/graph-ts"
+
+export const FACTORY_ADDRESS = '0x9Ad6C38BE94206cA50bb0d90783181662f0Cfa10'
+export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
+export let ZERO_BI = BigInt.fromI32(0)
+export let ONE_BI = BigInt.fromI32(1)
+export let ZERO_BD = BigDecimal.fromString('0')
+export let ONE_BD = BigDecimal.fromString('1')
