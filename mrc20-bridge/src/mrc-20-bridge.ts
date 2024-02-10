e93214e38d3fe0ac49ee444229c81737b6272343
Shayan Shiravani <shayan6959@gmail.com>
2024-02-10 20:02:08 +0330

    Initialize subgraph

---
+++ mrc20-bridge/src/mrc-20-bridge.ts
@@ -0,0 +1,105 @@
+import {
+  AddToken as AddTokenEvent,
+  Claim as ClaimEvent,
+  Deposit as DepositEvent,
+  RoleAdminChanged as RoleAdminChangedEvent,
+  RoleGranted as RoleGrantedEvent,
+  RoleRevoked as RoleRevokedEvent
+} from "../generated/MRC20Bridge/MRC20Bridge"
+import {
+  AddToken,
+  Claim,
+  Deposit,
+  RoleAdminChanged,
+  RoleGranted,
+  RoleRevoked
+} from "../generated/schema"
+
+export function handleAddToken(event: AddTokenEvent): void {
+  let entity = new AddToken(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.addr = event.params.addr
+  entity.tokenId = event.params.tokenId
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleClaim(event: ClaimEvent): void {
+  let entity = new Claim(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.user = event.params.user
+  entity.txId = event.params.txId
+  entity.fromChain = event.params.fromChain
+  entity.amount = event.params.amount
+  entity.tokenId = event.params.tokenId
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleDeposit(event: DepositEvent): void {
+  let entity = new Deposit(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.txId = event.params.txId
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleRoleAdminChanged(event: RoleAdminChangedEvent): void {
+  let entity = new RoleAdminChanged(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.role = event.params.role
+  entity.previousAdminRole = event.params.previousAdminRole
+  entity.newAdminRole = event.params.newAdminRole
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleRoleGranted(event: RoleGrantedEvent): void {
+  let entity = new RoleGranted(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.role = event.params.role
+  entity.account = event.params.account
+  entity.sender = event.params.sender
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleRoleRevoked(event: RoleRevokedEvent): void {
+  let entity = new RoleRevoked(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.role = event.params.role
+  entity.account = event.params.account
+  entity.sender = event.params.sender
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
---
+++ mrc20-bridge/tests/mrc-20-bridge.test.ts
@@ -0,0 +1,53 @@
+import {
+  assert,
+  describe,
+  test,
+  clearStore,
+  beforeAll,
+  afterAll
+} from "matchstick-as/assembly/index"
+import { Address, BigInt, Bytes } from "@graphprotocol/graph-ts"
+import { AddToken } from "../generated/schema"
+import { AddToken as AddTokenEvent } from "../generated/MRC20Bridge/MRC20Bridge"
+import { handleAddToken } from "../src/mrc-20-bridge"
+import { createAddTokenEvent } from "./mrc-20-bridge-utils"
+
+// Tests structure (matchstick-as >=0.5.0)
+// https://thegraph.com/docs/en/developer/matchstick/#tests-structure-0-5-0
+
+describe("Describe entity assertions", () => {
+  beforeAll(() => {
+    let addr = Address.fromString("0x0000000000000000000000000000000000000001")
+    let tokenId = BigInt.fromI32(234)
+    let newAddTokenEvent = createAddTokenEvent(addr, tokenId)
+    handleAddToken(newAddTokenEvent)
+  })
+
+  afterAll(() => {
+    clearStore()
+  })
+
+  // For more test scenarios, see:
+  // https://thegraph.com/docs/en/developer/matchstick/#write-a-unit-test
+
+  test("AddToken created and stored", () => {
+    assert.entityCount("AddToken", 1)
+
+    // 0xa16081f360e3847006db660bae1c6d1b2e17ec2a is the default address used in newMockEvent() function
+    assert.fieldEquals(
+      "AddToken",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
+      "addr",
+      "0x0000000000000000000000000000000000000001"
+    )
+    assert.fieldEquals(
+      "AddToken",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
+      "tokenId",
+      "234"
+    )
+
+    // More assert options:
+    // https://thegraph.com/docs/en/developer/matchstick/#asserts
+  })
+})
